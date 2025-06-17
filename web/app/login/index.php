6ae05011b1128425fe9a975bf81a899949812eef
Niklas Berger <mail@niklasberger.com>
2025-06-17 22:36:31 +0200

    Login page design

---
+++ web/app/login/index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sign In | Knownora</title>
+    <link rel="shortcut icon" href="../../favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="../../assets/style/general.css">
+    <link rel="stylesheet" href="../../assets/style/login.css">
+</head>
+<body>
+    <main>
+        <div id="login-logo">
+            <img src="../../assets/images/knownora.svg" alt="Knownora Icon">
+            <span>Knownora</span>
+        </div>
+        <div id="login-main">
+            <h1>Sign in to Example School</h1>
+            <form>
+                <label for="login-email">Email</label><br>
+                <input type="email" id="login-email" placeholder="your.name@example.edu"><br>
+                <label for="login-password">Password</label><br>
+                <input type="password" id="login-password" placeholder="••••••••••">
+                <button type="submit" class="simple-button" id="login-button">Continue</button>
+            </form>
+            <div>
+                <a href="./forgot-password" class="underlined-link">I forgot my password<div></div></a>
+            </div>
+            <div>
+                <a href="./create-account" class="underlined-link">Create an account<div></div></a>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
