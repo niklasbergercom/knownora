ad6afeb54950e93b5f60ca11c1125b86cfefa597
Niklas Berger <mail@niklasberger.com>
2025-06-18 19:47:16 +0200

    Design for forgot-password and header on main app

---
+++ web/app/index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome | Knownora</title>
+    <link rel="shortcut icon" href="../favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="../assets/style/general.css">
+    <link rel="stylesheet" href="../assets/style/app.css">
+</head>
+<body>
+    <header>
+        <div>
+            <img src="../assets/images/knownora.svg" alt="Knownora Icon">
+            <ul id="header-menu-left">
+                <li><a class="underlined-link">Home<div></div></a></li>
+                <li><a class="underlined-link">Chat<div></div></a></li>
+                <li><a class="underlined-link">Calendar<div></div></a></li>
+            </ul>
+        </div>
+        <div>
+            <div id="my-profile-button-wrapper">
+                <img src="https://cdn.niklasberger.com/knownora/example-pictures/male01.jpg" alt="Your Profile" title="Your Profile">
+            </div>
+        </div>
+    </header>
+    <main></main>
+</body>
+</html>
